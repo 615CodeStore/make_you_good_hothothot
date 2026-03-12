a54ffdf06b483f070b40317e95eb1be21667c2a3
mrlihandsomeb <2774768603@qq.com>
2026-03-12 10:30:38 +0800

    fix bug
    解决了闪烁问题

--- practice/mainpart.cpp
+++ practice/mainpart.cpp
@@ -3,47 +3,43 @@
 #include <Windows.h>
 
 
-IMAGE heart,heartmask;
+IMAGE heart, heartmask;
 
 void begin1()
 {
-	loadimage(&heart, "image/heart.jpg");
-	loadimage(&heartmask, "image/heartmask.jpg");
+	loadimage(&heart, L"image/heart.jpg");
+	loadimage(&heartmask, L"image/heartmask.jpg");
 	for (int i = 10; i < 750;)
 	{
-		bool color=0;
+		bool color = 0;
+		cleardevice();
 		setfillcolor(WHITE);
-		solidcircle(640, 360, i);		 putimage(220, 90, &heartmask,NOTSRCERASE); putimage(220, 90, &heart,SRCINVERT);
-		FlushBatchDraw();
+		solidcircle(640, 360, i);		 putimage(220, 90, &heartmask, NOTSRCERASE); putimage(220, 90, &heart, SRCINVERT);
 		for (int j = i; j >= 10; j -= 15)
 		{
 			if (color == 0)
 			{
 				setfillcolor(MAGENTA);
 				solidcircle(640, 360, j);		 putimage(220, 90, &heartmask, NOTSRCERASE); putimage(220, 90, &heart, SRCINVERT);
-				color=1;
-				FlushBatchDraw();
+				color = 1;
 			}
 			else
 			{
 				setfillcolor(WHITE);
 				solidcircle(640, 360, j);		 putimage(220, 90, &heartmask, NOTSRCERASE); putimage(220, 90, &heart, SRCINVERT);
-				color=0;
-				FlushBatchDraw();
+				color = 0;
 			}
 		}
-		Sleep(7);
-		cleardevice();
 		FlushBatchDraw();
 		if (i < 150)
 		{
-			i ++;
+			i++;
 		}
-		else if (i <300 )
+		else if (i < 300)
 		{
 			i += 2;
 		}
-		else if(i<400)
+		else if (i < 400)
 		{
 			i += 3;
 		}
@@ -55,17 +51,18 @@ void begin1()
 		{
 			i += 5;
 		}
+		Sleep(7);
 	}
 }
 int main()
 {
-	int x = 640, y =360,size=10;
+	int x = 640, y = 360, size = 10;
 	initgraph(1280, 720);
-	//BeginBatchDraw();
+	BeginBatchDraw();
 	while (true)
 	{
 		begin1();
 	}
-	//EndBatchDraw();
+	EndBatchDraw();
 	return 0;
 }
